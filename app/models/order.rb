54ddf35c3b915f78cd6ec155aba5a87e533caab1
Tyler Niswonger <tylerniswonger@gmail.com>
2020-03-08 14:20:56 -0500

    Added nested forms

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,9 @@ class Order < ApplicationRecord
   belongs_to :Order_Status
   belongs_to :Customer
   belongs_to :Rental_Order
+  has_many :order_lines
+  has_many :rental_lines
+
+  accepts_nested_attributes_for :order_lines, reject_if: :all_blank, allow_destroy: true
+  accepts_nested_attributes_for :rental_lines, reject_if: :all_blank, allow_destroy: true
 end
