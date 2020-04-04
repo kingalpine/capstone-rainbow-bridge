54bc089e56a3955930ae8895b530b1af1a260940
Hudson <hrlaplant95@gmail.com>
2020-04-04 03:50:19 -0500

    Sorting orders, customers, order lines employees

--- app/controllers/order_lines_controller.rb
+++ app/controllers/order_lines_controller.rb
@@ -1,10 +1,11 @@
 class OrderLinesController < ApplicationController
-  before_action :set_order_line, only: [:show, :edit, :update, :destroy]
-
-  # GET /order_lines
-  # GET /order_lines.json
+  before_action :set_order, only: [:show, :edit, :update, :destroy]
+  helper_method :sort_column, :sort_direction
+  require 'date'
+  # GET /orders
+  # GET /orders.json
   def index
-    @order_lines = OrderLine.all
+    @pagy,@order_lines = pagy(OrderLine.all.order(sort_column + ' ' + sort_direction))
   end
 
   # GET /order_lines/1
@@ -72,4 +73,12 @@ class OrderLinesController < ApplicationController
     def order_line_params
       params.require(:order_line).permit(:order_line_description, :order_line_start_date, :order_line_finish_date, :special_order_notes, :product_id, :order_line_status_id, order_line_restrictions_attributes: [:order_line_id, :dietary_restriction_id, :_destroy], tasks_attributes: [:task_name, :task_description, :task_start_date, :task_due_date, :task_finish_date, :task_status_id, :_destroy], tiers_attributes: [:position, :tier_size, :tier_special_notes, :cake_flavor_id, :frosting_flavor_id, :shape_id, :_destroy])
     end
+    def sort_column
+      OrderLine.column_names.include?(params[:sort]) ? params[:sort] : "order_line_description"
+    end
+    
+    def sort_direction
+      %w[asc desc].include?(params[:direction]) ?  params[:direction] : "asc"
+    end
 end
+
